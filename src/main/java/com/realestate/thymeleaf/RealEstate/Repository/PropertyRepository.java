e52b6e0150995240c56a094e192af39d774822ba
Howard <howard_soon@outlook.com>
2024-06-15 17:24:05 +0200

    jpa

--- src/main/java/com/realestate/thymeleaf/RealEstate/Repository/PropertyRepository.java
+++ src/main/java/com/realestate/thymeleaf/RealEstate/Repository/PropertyRepository.java
@@ -1,7 +1,6 @@
 package com.realestate.thymeleaf.RealEstate.Repository;
 
 import com.realestate.thymeleaf.RealEstate.Model.PropertyData;
-import com.realestate.thymeleaf.RealEstate.Model.UserData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PropertyRepository extends JpaRepository<PropertyData,Long> {
