495a8b59b270d75ba6fb33946783bd0ed0d64be2
dariacherepina <cherepinadara@gmail.com>
2024-07-05 17:03:46 +0200

    rent appartment works

--- src/main/java/com/realestate/thymeleaf/RealEstate/Controllers/BuyerPageController.java
+++ src/main/java/com/realestate/thymeleaf/RealEstate/Controllers/BuyerPageController.java
@@ -4,20 +4,25 @@ package com.realestate.thymeleaf.RealEstate.Controllers;
 import com.realestate.thymeleaf.RealEstate.Model.PropertyData;
 import com.google.gson.Gson;
 import com.realestate.thymeleaf.RealEstate.Repository.PropertyRepository;
+import com.realestate.thymeleaf.RealEstate.Service.SoldPropertiesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class BuyerPageController {
     private final PropertyRepository propertyRepository;
+    private final SoldPropertiesService soldPropertiesService;
 
     @Autowired
-    public BuyerPageController(PropertyRepository propertyRepository) {
+    public BuyerPageController(PropertyRepository propertyRepository, SoldPropertiesService soldPropertiesService) {
         this.propertyRepository = propertyRepository;
+        this.soldPropertiesService = soldPropertiesService;
     }
 
     @GetMapping("/BuyerPage")
@@ -40,5 +45,16 @@ public class BuyerPageController {
         model.addAttribute("jsonContactProperties", jsonContactProperties);
 
         return "buyerPage";
+    }
+    @PostMapping(value = "/buyerPage", params = {"propertyId", "buyerEmail"})
+    public String buyProperty(@RequestParam("propertyId") Long propertyId, @RequestParam("buyerEmail") String buyerEmail, Model model) {
+        try {
+            soldPropertiesService.sellProperty(propertyId, buyerEmail);
+            model.addAttribute("message", "Property purchased successfully.");
+            return "redirect:/BuyerPage";
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("error", e.getMessage());
+            return "buyerPage";
+        }
     }
 }
